820da137919addfa246f97ec051c4ad80416e16b
silvanletsch <93323390+silvanletsch@users.noreply.github.com>
2023-04-18 15:44:58 +0200

    h

--- index.php
+++ index.php
@@ -3,38 +3,3 @@
 include_once 'backend/login_check.php';
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="frontend/style.css">
-    <title>Excursieblog</title>
-</head>
-<body>
-    <img id="logo" src="images/logo.png" alt="">
-    <div id="links">
-    
-        <a href="index.php">Home</a>
-        <a href="frontend/create.php">Create blog</a>
-        <a href="frontend/browse.php">Browse blogs</a>
-
-        <a href="backend/login.php">Login</a>
-        <a href="backend/uitlog.php">Uitlog</a>
-        <a href="backend/registreren.php">Register</a>
-    </div>
-    <hr />
-    <div id="body-container">
-        <div class="indexwrapper">
-        <h1>Welkom bij de Excursieblog van het GLR!</h1>
-        <a href="browse.php">Blogs bekijken</a>
-        <a href="create.php">Blog Toevoegen</a>
-        </div>
-        <img id="indexbg" src="images/indexbackground.png">
-    </div>
-    <footer>
-
-    </footer>
-</body>
-</html>
