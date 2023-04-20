8c85bc37a1b45b324573afbc0de8a544d79e2893
Oliver <oliver.boros.nl@gmail.com>
2023-04-20 15:41:23 +0200

    Still wiping createa.php

--- frontend/create.php
+++ frontend/create.php
@@ -25,47 +25,101 @@ if ($result->num_rows > 0) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>create</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Landing</title>
+
+    <link rel="stylesheet" href="index.css" />
 </head>
+
 <body>
-    <img id="logo" src="images/logo.png" alt="">
-    <div id="links">
-    
-        <a href="../index.php">Home</a>
-        <a href="create.php">Create blog</a>
-        <a href="browse.php">Browse blogs</a>
+    <!-- + navbar -->
+    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="index.html">
+                <img src="logo.svg" alt="logo" width="28" />
+            </a>
+            <a role="button" class="navbar-burger" id="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+        <!-- TODO: hamburg links goed zetten -->
+        <!-- ?: wtf moet eig in de hamburg komen? -->
+        <div id="navbar" class="navbar-menu">
+            <div class="navbar-start">
+                <a href="projects.html" class="navbar-item"> Projects </a>
+            </div>
+        </div>
+    </nav>
+    <!-- ^ navbar -->
 
-        <a href="../backend/login.php">Login</a>
-        <a href="../backend/uitlog.php">Uitlog</a>
-        <a href="../backend/registreren.php">Register</a>
+    <!-- + links -->
+    <div class="tabs is-right">
+        <ul>
+            <li><a>Toevoeg</a></li>
+            <li><a>Uitlog</a></li>
+            <li><a>Overzicht</a></li>
+        </ul>
     </div>
-    <hr />
-    <div id="body-container">
+    <!-- ^ links -->
+
+    <!-- + input -->
+    <div class="columns box m-3 p-1">
+        <div id="file-input" class="column file has-name is-fullwidth">
+            <label class="file-label">
+                <input id="fileInput" class="file-input" type="file" name="resume" />
+                <span class="file-cta">
+                    <span class="file-icon">
+                        <i class="fas fa-upload"></i>
+                    </span>
+                    <span class="file-label"> Choose a file… </span>
+                </span>
+                <span class="file-name"> No file selected. </span>
+            </label>
+        </div>
+        <div class="column field is-grouped is-grouped-centered">
+            <p class="control">
+                <a class="button is-primary"> <input type="submit" /> </a>
+            </p>
+            <p id="clear" class="control">
+                <a class="button is-danger"> Clear </a>
+            </p>
+        </div>
+    </div>
+
+    <!-- ^ input -->
+
+
+
     <form action="../backend/createverwerk.php" method="post" enctype="multipart/form-data">
-            <label for="title">Title: </label></br>
-            <input type="text" name="title" id="title" maxlength="15"></br>
+        <label for="title">Title: </label></br>
+        <input type="text" name="title" id="title" maxlength="15"></br>
 
-            <label for="image">Image: </label></br>
-            <input type="file" name="image" id="image" required></br>
+        <label for="image">Image: </label></br>
+        <input type="file" name="image" id="image" required></br>
 
-            <input type="hidden" name="user" id="user" value="<?php $_SESSION['id'] ?>" ></br>
+        <input type="hidden" name="user" id="user" value="<?php $_SESSION['id'] ?>"></br>
 
-            <label for="group">Group: </label></br>
-            <select name="group" id="group">
-                <?php foreach ($groups as $group) { ?>
-                    <option value="<?php echo $group['ID']; ?>"><?php echo $group['name']; ?></option>
-                <?php } ?>
-            </select></br>
+        <label for="group">Group: </label></br>
+        <select name="group" id="group">
+            <?php foreach ($groups as $group) { ?>
+                <option value="<?php echo $group['ID']; ?>"><?php echo $group['name']; ?></option>
+            <?php } ?>
+        </select></br>
 
-            <input type="submit">
-        </form>
-    </div>
-    <footer>
-    </footer>
+        <input type="submit" />
+    </form>
+
+    
+    <!-- + scripts -->
+    <script src="./bulma-dropdown.js" defer></script>
+    <script src="./bulma-fileInput.js"></script>
+    <!-- ^scripts -->
 </body>
+
 </html>
