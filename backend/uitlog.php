6d143702d4b7e4e15a13d8af04a12a1621a355d5
silvanletsch <93323390+silvanletsch@users.noreply.github.com>
2023-04-21 12:10:32 +0200

    kut voor crud

--- backend/uitlog.php
+++ backend/uitlog.php
@@ -5,5 +5,5 @@ require_once 'login_check.php';
 echo "U bent uiteglogd!<br>";
 echo "<a href=' index.php'>OVERZICHT</a>";
 session_destroy();
-header('Location: index.php');
+header('Location: https://87957.stu.sd-lab.nl/Upload-Foto-Telefoon-Beroeps/backend/login.php');
 
--- crud/uitlog.php
+++
@@ -1,8 +0,0 @@
-<link rel="stylesheet" href="style/style.css">
-
-<?php
-require_once 'login_check.php';
-echo "U bent uiteglogd!<br>";
-echo "<a href='toonagenda.php'>OVERZICHT</a>";
-session_destroy();
-
